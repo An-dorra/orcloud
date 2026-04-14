8e4b7b0ee35703b027cb8591c33585461a7f7e69
Andorra <an>
2026-04-14 16:24:02 +0800

    feat: add full-bleed layout utilities

---
+++ src/components/layout/FullBleedSection.vue
@@ -0,0 +1,23 @@
+<template>
+  <component :is="tag" class="full-bleed" :class="outerClass">
+    <div class="full-bleed__inner" :class="innerClass">
+      <slot />
+    </div>
+  </component>
+</template>
+
+<script setup lang="ts">
+interface FullBleedSectionProps {
+  tag?: keyof HTMLElementTagNameMap
+  outerClass?: string
+  innerClass?: string
+}
+
+const props = withDefaults(defineProps<FullBleedSectionProps>(), {
+  tag: 'section',
+  outerClass: '',
+  innerClass: '',
+})
+
+const { tag, outerClass, innerClass } = props
+</script>
